06aa0bd1f2d44671a669a4e56827a7ed0447c5dd
Remi Burgel <remi.burgel@iex.ec>
2021-04-28 11:35:22 +0200

    Adding a test for the whole dapp

--- tests/dapp.test.js
+++ tests/dapp.test.js
@@ -18,11 +18,11 @@ describe('dapp', () => {
       }),
     }));
     process.env.IEXEC_INPUT_FILES_FOLDER = './tests/test_files';
-    process.env.IEXEC_INPUT_FILE_NAME_1 = 'input_file_no_dataset.json';
+    process.env.IEXEC_INPUT_FILE_NAME_1 = 'input_file.json';
     process.env.IEXEC_OUT = './tests/test_out';
     process.env.IEXEC_NB_INPUT_FILES = '1';
     process.env.IEXEC_IN = './tests/test_files';
-    process.env.IEXEC_DATASET_FILENAME = 'dataset.json';
+    process.env.IEXEC_DATASET_FILENAME = 'full-dataset.json';
     process.env.IEXEC_DATASET_ADDRESS = '0x0000000000000000000000000000000000000001';
 
     await dapp();
