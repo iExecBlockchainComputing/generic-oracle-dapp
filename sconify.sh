85f5f9d1dd4cf9e038fc9d20a6e9a9b80f8d3023
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2021-07-23 16:39:28 +0200

    update buid script

--- sconify.sh
+++ sconify.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# Until the correct image is made available, do this workaround:
-# docker image pull registry.scontain.com:5050/sconecuratedimages/iexec:node-14.4.0-alpine3.11
-# docker image tag registry.scontain.com:5050/sconecuratedimages/iexec:node-14.4.0-alpine3.11 registry.scontain.com:5050/sconecuratedimages/node:14.4.0-alpine3.11
+# declare the app entrypoint
+ENTRYPOINT="node /app/app.js"
+# declare an image name
+IMG_NAME=generic-oracle
+
+IMG_FROM=${IMG_NAME}:temp-non-tee
+IMG_TO=${IMG_NAME}:tee-debug
+
+# build the regular non-TEE image
+docker build . -t ${IMG_FROM}
+
+# pull the SCONE curated image corresponding to our base image
+docker pull registry.scontain.com:5050/sconecuratedimages/node:14.4.0-alpine3.11
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/sconecuratedimages/iexec-sconify-image:5.3.3 \
+            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.7 \
             sconify_iexec \
-                --name=nodejsHelloWorld \
-                --from=nexus.iex.ec/generic-oracle-dapp:1.0.0-alpha \
-                --to=nexus.iex.ec/generic-oracle-dapp:1.0.0-tee-alpha \
+                --name=${IMG_NAME} \
+                --from=${IMG_FROM} \
+                --to=${IMG_TO} \
                 --binary-fs \
                 --fs-dir=/app \
                 --host-path=/etc/hosts \
                 --host-path=/etc/resolv.conf \
-                --binary="/usr/local/bin/node" \
-                --heap="1G" \
-                --dlopen="2" \
+                --binary=/usr/local/bin/node \
+                --heap=1G \
+                --dlopen=2 \
                 --no-color \
-                --verbose \
-                --command="node /app/app.js"
+                --command="node /app/app.js" \
+                && echo -e "\n------------------\n" \
+                && echo "successfully built TEE docker image => ${IMG_TO}" \
+                && echo "application mrenclave.fingerprint is $(docker run -it --rm -e SCONE_HASH=1 ${IMG_TO})"
