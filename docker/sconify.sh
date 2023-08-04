a35a6a55e959a91eea3a854f334084ee43c3c828
Le-Caignec <robinlecaignec@yahoo.fr>
2023-08-04 10:58:26 +0200

    add build_args in drone pipeline

--- docker/sconify.sh
+++ docker/sconify.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
-# IMG_FROM=docker.io/iexechub/generic-oracle-dapp:dev \
-#     IMG_TO=docker.io/iexechub/generic-oracle-dapp:dev-debug \
-#     ./sconify.sh
 
-cd $(dirname $0)
+# declare the app entrypoint
+ENTRYPOINT="node /app/app.js"
+# declare an image name
+IMG_NAME=generic-oracle-dapp
 
-ARGS=$(sed -e "s'\${IMG_FROM}'${IMG_FROM}'" -e "s'\${IMG_TO}'${IMG_TO}'" sconify.args)
-echo $ARGS
+IMG_FROM=${IMG_NAME}:temp-non-tee
+IMG_TO=${IMG_NAME}:tee-debug
 
-/bin/bash -c "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.15 \
-            sconify_iexec $ARGS"
+# build the regular non-TEE image
+docker build . -t ${IMG_FROM}
+
+# pull the SCONE curated image corresponding to our base image
+docker pull registry.scontain.com:5050/sconecuratedimages/node:14.4.0-alpine3.11
+
+docker run -it --rm \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.7 \
+            sconify_iexec \
+                --name=${IMG_NAME} \
+                --from=${IMG_FROM} \
+                --to=${IMG_TO} \
+                --binary-fs \
+                --fs-dir=/app \
+                --host-path=/etc/hosts \
+                --host-path=/etc/resolv.conf \
+                --binary=/usr/local/bin/node \
+                --heap=1G \
+                --dlopen=2 \
+                --no-color \
+                --command="node /app/app.js" \
+                && echo -e "\n------------------\n" \
+                && echo "successfully built TEE docker image => ${IMG_TO}" \
+                && echo "application mrenclave.fingerprint is $(docker run -it --rm -e SCONE_HASH=1 ${IMG_TO})"
